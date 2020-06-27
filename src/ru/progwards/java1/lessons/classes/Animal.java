edad418b32827e3dd3079060e1551977cebd550f
kas <kadyrovas@yandex.ru>
2020-06-27 11:50:03 +0300

    Home work ru.progwards.java1.lessons.classes 2 попытка

--- src/ru/progwards/java1/lessons/classes/Animal.java
+++ src/ru/progwards/java1/lessons/classes/Animal.java
@@ -10,13 +10,11 @@ public class Animal {
     public AnimalKind getKind() {
         return AnimalKind.ANIMAL;
     }
-    public FoodKind getFoodKind() {
-        return FoodKind.UNKNOWN;
-    }
+    public FoodKind getFoodKind() {return FoodKind.UNKNOWN;}
 
     @Override
     public String toString() {
-        return "I am " + getKind() + ", eat " + getClass();
+        return "I am " + getKind() + ", eat " + getFoodKind();
     }
     public double getWeight() {return weight;}
     public double getFoodCoeff() {return 0.02;}
