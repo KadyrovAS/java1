40167887270d6e49ba0783ede7746e8ee2212506
Andrey <kadyrovas@yandex.ru>
2020-07-03 13:21:04 +0300

    10

--- src/ru/progwards/java1/lessons/interfaces/Animal.java
+++ src/ru/progwards/java1/lessons/interfaces/Animal.java
@@ -52,10 +52,21 @@ public class Animal implements FoodCompare, CompareWeight {
         return Double.compare(this.getFoodPrice(), aminal.getFoodPrice());
     }
 
+    @Override
+    public CompareResult compareWeight(CompareWeight smthHasWeigt) {
+        int res;
+        Animal smthAnimal = (Animal) smthHasWeigt;
+        res = Double.compare(this.getWeight(), smthAnimal.getWeight());
+        if (res < 0) {
+            return CompareResult.LESS;
+        } else if (res == 0) return CompareResult.EQUAL;
+        else return CompareResult.GREATER;
+    }
+
+
     public static void main(String[] args) {
         Animal animal1 = new Animal(100);
         Animal animal2 = new Animal(200);
-        System.out.println(animal1.compareWeight(animal2));
         System.out.println(animal1.getFood1kgPrice());
     }
 }
