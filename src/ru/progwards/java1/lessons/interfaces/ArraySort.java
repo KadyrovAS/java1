403b7e3090c2cc145bccddf29d6283d1b0a340f6
Andrey <kadyrovas@yandex.ru>
2020-07-08 15:47:04 +0300

    Interfaces 01

--- src/ru/progwards/java1/lessons/interfaces/ArraySort.java
+++ src/ru/progwards/java1/lessons/interfaces/ArraySort.java
@@ -1,43 +1,14 @@
 package ru.progwards.java1.lessons.interfaces;
 
-public class ArraySort implements CompareWeight{
-    private int weight;
-
-    public ArraySort(int weight) {
-        this.weight = weight;
-    }
-
-    public int getWeight() {
-        return this.weight;
-    }
-
-    @Override
-    public CompareResult compareWeight(CompareWeight smthHasWeigt) {
-        int res;
-        ArraySort smthFood;
-        smthFood = (ArraySort) smthHasWeigt;
-        res = Double.compare(this.getWeight(), smthFood.getWeight());
-        if (res < 0) {
-            return CompareResult.LESS;
-        } else if (res == 0) return CompareResult.EQUAL;
-        else return CompareResult.GREATER;
-    }
-
+public class ArraySort{
     public static void sort(CompareWeight[] a) {
-        //Я не совсем понял этот момент. CompareWeight - это метод, а не каласс.
-        //И в интерфейсе он описан как метод. Как понять такой тип передаваемого аргумента?
-
         CompareWeight s;
-        ArraySort ai, aj;
+        Animal ai, aj;
         for (int i = 0; i < a.length - 1; i++) {
             for (int j = i + 1; j < a.length; j++) {
-                //Не нашел другого варианта переопределить массив
-                //(ArraSort) a - не пропускает
-                //(ArraySort) a[i] тоже ругается
-
-                ai = (ArraySort) a[i];
-                aj = (ArraySort) a[j];
-                if (aj.compareWeight(ai) == CompareResult.LESS) {
+                ai = (Animal) a[i];
+                aj = (Animal) a[j];
+                if (aj.getWeight() < ai.getWeight()) {
                     s = a[i];
                     a[i] = a[j];
                     a[j] = s;
@@ -46,15 +17,4 @@ public class ArraySort implements CompareWeight{
         }
     }
 
-    public static void main(String[] args) {
-        ArraySort[] arWeight = new ArraySort[10];
-        for (int i = 0; i < 10; i++) {
-            arWeight[i] = new ArraySort(100 - i);
-        }
-        sort(arWeight);
-        for (int i = 0; i < 10; i++) {
-            System.out.println(arWeight[i].weight);
-        }
-
-    }
 }
