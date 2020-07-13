02cf0875a1a7b80c8f2a33e46a429e6532e5dc74
Andrey <kadyrovas@yandex.ru>
2020-07-13 14:01:37 +0300

    ru.progwards.java1.lessons.io 08

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -1,28 +1,9 @@
 package ru.progwards.java1.lessons.io1;
 
 import java.io.*;
-import java.util.Arrays;
-
-//Уважаемый, Сергей.
-//Не могу понять, почему робот ругается на этот код.
-//У меня в IDEA все работает корректно. Файл кодируется
-//Подскажите пожалуйста, что я не так пробросил?
-
-//И с этим кодом у меня еще появился вопрос.
-//Если я каждый оператор, который потенциально может вызвать исключение, буду перекрывать try catch,
-//то код будет нечитаемым. Поэтому, скорее всего, требуется обработка исключений в блоке catch.
-//Например, в моем случае некоторые файлы могут быть по-просту не открыты, а я их в finally пытаюсь закрыть
-//без всякой проверки.
-//Может быть из-за этого на меня робот ругается?
 
 public class Coder {
-    public static void codeFile(String inFileName, String outFileName, char[] code, String logName)
-            throws IOException  {
-        System.out.println("inFileName = " + inFileName);
-        System.out.println("outFileName = " + outFileName);
-        System.out.println(Arrays.toString(code));
-        System.out.println("code.length = " + code.length);
-        System.out.println("logName" + logName);
+    public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
 
         FileReader fileReader = null;
         FileWriter fileWriter = null;
@@ -42,7 +23,7 @@ public class Coder {
                     intCode = reader.read();
                     if (intCode != -1) {
                         fileWriter.write(code[intCode]);
-                        System.out.println(intCode);
+
                 }
                 }
             } catch (IOException e) {
@@ -60,8 +41,9 @@ public class Coder {
 
         }
 
-    public static void main(String[] args) throws IOException {
-        int n = 65535;
+    public static void main(String[] args) {
+        int n = Character.MAX_VALUE;
+
         char[] code = new char[n];
         for (int i = 0; i < n; i ++) {
         code[n - i - 1] = (char) i;
