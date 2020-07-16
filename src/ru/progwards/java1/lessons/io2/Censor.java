71e5fb38f112479bf97c93d9a83bfd344e27ab95
Andrey <kadyrovas@yandex.ru>
2020-07-16 23:49:09 +0300

    ru.progwards.java1.lessons.io2; 5

--- src/ru/progwards/java1/lessons/io2/Censor.java
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -2,12 +2,6 @@ package ru.progwards.java1.lessons.io2;
 
 import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
-
-//Думаю, что цель была освоить RandomAccessFile. Я его освоил. Проблема возникла с кодировками.
-//Прочитать и раскодировать файл у меня получилось, но вот записать обратно в той кодировке, что
-//была, не вышло. Поэтому я пошел по пути создания дополнительного файла, который в конце работы
-//просто удаляю
 
 public class Censor {
 
@@ -26,45 +20,39 @@ public class Censor {
         }
     }
 
-    public static void censorFile(String inoutFileName, String[] obscene) throws CensorException {
-
-         String[] obsceneCopy = new String[obscene.length];
+    private static String[] copyStar(String[] obscene) {
+        //Создание obsceneCopy, заполненного "*", кол-во которых равно длине соответствующего элемента obscene
+        String[] obsceneCopy = new String[obscene.length];
         StringBuilder copyString = new StringBuilder("");
         int i = 0;
         for (String word : obscene) {
-            //Создание obseneCopy, соответствующего obscene, заполненного "*"
             if (copyString.length() > 0) copyString.delete(0, copyString.length());
             for (int k = 0; k < obscene[i].length(); k++) copyString.append("*");
             obsceneCopy[i] = copyString.toString();
             i++;
         }
-        try {
-            FileReader fileReader = new FileReader(inoutFileName);
-            FileWriter fileWriter = new FileWriter("2" + inoutFileName);
-            String currentLine;
-            try (Scanner scanner = new Scanner(fileReader);) {
-                while (scanner.hasNext()) {
-                    currentLine = scanner.nextLine();
-                    for (i = 0; i < obscene.length; i++) {
-                        if (currentLine.contains(obscene[i]))
-                            currentLine = currentLine.replaceAll(obscene[i], obsceneCopy[i]);
-                    }
-                    fileWriter.write(currentLine + "\n");
-                }
+        return obsceneCopy;
+    }
 
+    public static void censorFile(String inoutFileName, String[] obscene) throws CensorException {
+        String[] obsceneCopy = copyStar(obscene);
+        String currentLine;
+        long startPosition;
+        long endPosition;
 
-                if (fileWriter != null) fileWriter.close();
-                if (fileReader != null) fileReader.close();
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(inoutFileName, "rw");) {
+        while (randomAccessFile.getFilePointer() < randomAccessFile.length()) {
+            startPosition = randomAccessFile.getFilePointer();
+            currentLine = randomAccessFile.readLine();
+            endPosition = randomAccessFile.getFilePointer();
+            for (int i = 0; i < obscene.length; i ++)
+                if (currentLine.contains(obscene[i]))
+                    currentLine = currentLine.replaceAll(obscene[i], obsceneCopy[i]);
+            randomAccessFile.seek(startPosition);
+            randomAccessFile.writeBytes(currentLine);
+            randomAccessFile.seek(endPosition);
+        }
 
-                File fileChanged = new File(inoutFileName);
-                if (fileChanged.delete()) {
-                    File fileCopy = new File("2" + inoutFileName);
-                    fileCopy.renameTo(fileChanged);
-                }
-            } catch (Throwable e) {
-                CensorException censorException = new CensorException(e.getMessage(), inoutFileName);
-                throw censorException;
-            }
         } catch (IOException e) {
             CensorException censorException = new CensorException(e.getMessage(), inoutFileName);
             throw censorException;
@@ -73,13 +61,12 @@ public class Censor {
 
 
     public static void main(String[] args) {
-        String[] line = {"друг", "взоры", "Пора", "Мороз"};
+        String[] line = {"two", "storey", "write", "count", "day"};
         try {
             censorFile("myFirstFile.txt", line);
         } catch (Throwable e) {
             System.out.println(e);
         }
-
 
     }
 
