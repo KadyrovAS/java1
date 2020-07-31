5680c08ff3b6b6ea99ebbc74a077eeafcdb7b399
Andrey <kadyrovas@yandex.ru>
2020-07-31 11:42:12 +0300

    ru.progwards.java1.lessons.datetime;15

--- src/ru/progwards/java1/lessons/datetime/Insurance.java
+++ src/ru/progwards/java1/lessons/datetime/Insurance.java
@@ -43,11 +43,12 @@ public class Insurance {
             this.duration = Duration.ofMillis(milisec);
         } else if (style == FormatStyle.LONG) {
             ldt = LocalDateTime.parse(strDuration, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+
             this.duration = Duration.ofHours(ldt.getHour())
-                                    .plusMinutes(ldt.getMinute())
-                                    .plusSeconds(ldt.getSecond())
-                                    .plusDays(ldt.getDayOfMonth())
-                                    .plusDays(ldt.getMonthValue() * 30);
+                    .plusMinutes(ldt.getMinute())
+                    .plusSeconds(ldt.getSecond())
+                    .plusDays(ldt.getDayOfMonth())
+                    .plusDays(ldt.getMonthValue() * 30);
         } else
             this.duration = Duration.parse(strDuration);
     }
@@ -67,17 +68,5 @@ public class Insurance {
             return "Insurance issued on " + this.start.toString() +  " is not valid";
     }
 
-    public static void main(String[] args) {
-        String strDuration = "0000-01-02T03:04:05";
-        LocalDateTime ldt = LocalDateTime.parse(strDuration, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-        Period period = Period.of(ldt.getYear(), ldt.getMonthValue(), ldt.getDayOfMonth());
 
-        Duration duration = Duration.ofHours(ldt.getHour())
-                 .plusMinutes(ldt.getMinute())
-                 .plusSeconds(ldt.getSecond())
-                .plusDays(ldt.getDayOfMonth())
-                .plusDays(ldt.getMonthValue() * 30);
-
-        System.out.println(duration.toSeconds());
-    }
 }
