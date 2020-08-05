4979705bacce1c2a8da661d9e071b25b16f19638
Andrey <kadyrovas@yandex.ru>
2020-08-05 08:46:22 +0300

    ru.progwards.java1.lessons.files;

--- src/ru/progwards/java1/lessons/files/OrderItem.java
+++ src/ru/progwards/java1/lessons/files/OrderItem.java
@@ -4,9 +4,7 @@ public class OrderItem {
     public String googsName; //наименование товара
     public int count; //количество
     public double price; //цена за единицу
-    public OrderItem (String googsName, int count, double price) {
-        System.out.println("OrderItem() googsName = '" + googsName+ "'; " +
-                "count = " + count + " price = " + price);
+    OrderItem (String googsName, int count, double price) {
         this.googsName = googsName;
         this.count = count;
         this.price = price;
