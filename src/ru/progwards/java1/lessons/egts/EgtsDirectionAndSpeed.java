5e8428ecfa0f22fafbfbbe3be99db90819db70c6
Andrey <kadyrovas@yandex.ru>
2020-08-13 17:47:24 +0300

    package ru.progwards.java1.lessons.abstractnum;

--- src/ru/progwards/java1/lessons/egts/EgtsDirectionAndSpeed.java
+++ src/ru/progwards/java1/lessons/egts/EgtsDirectionAndSpeed.java
@@ -9,7 +9,7 @@ public class EgtsDirectionAndSpeed {
 
     public static int getDirection(byte dirLow, short speedAndDir) {
         int intDirLow = dirLow & 0b11111111;
-        if ((speedAndDir & 0b1000_0000) == 0b1) intDirLow += 256;
+        if ((speedAndDir & 0b1000_0000) == 0b1) intDirLow += 512;
         return intDirLow;
     }
 
