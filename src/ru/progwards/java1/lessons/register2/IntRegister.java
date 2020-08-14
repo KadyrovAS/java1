7dd6ad7037127dcb0109f56bd595337b09887248
Andrey <kadyrovas@yandex.ru>
2020-08-14 10:58:39 +0300

    package ru.progwards.java1.lessons.abstractnum;

--- src/ru/progwards/java1/lessons/register2/IntRegister.java
+++ src/ru/progwards/java1/lessons/register2/IntRegister.java
@@ -2,62 +2,18 @@ package ru.progwards.java1.lessons.register2;
 
 public class IntRegister extends Register {
 
-    Bit[] bits = new Bit[32];
     public IntRegister() {
+        bits = new Bit[32];
         for (int i = 0; i < 32; i ++) this.bits[i] = new Bit(false);
     }
     public IntRegister(int value) {
         int sym;
+        bits = new Bit[32];
         for (int i = 0; i < 32; i ++) {
             if ((value & 1) == 1) this.bits[31-i] = new Bit(true);
             else this.bits[31-i] = new Bit(false);
             value >>= 1;
         }
-    }
-
-    @Override
-    public void putRegister(Bit[] value)   {
-        int n = value.length;
-        for (int i = 0; i < n; i ++) this.bits[i] = value[i];
-    }
-
-    @Override
-    public Bit[] getBits() {
-        return this.bits;
-    }
-
-
-    @Override
-    public String toString() {
-        String result = "";
-        for (Bit bit: this.bits) result += bit.toString();
-        return result;
-    }
-
-    @Override
-    public String toDecString() {
-        int result = 0;
-        int n = this.bits.length;
-        for (int i = 0; i < n; i ++){
-            result += Integer.valueOf(this.bits[i].toString()) * n2Mult(n - i - 1);
-        }
-        return String.valueOf(result);
-    }
-
-    public Register toTwosComplement(Register value) {
-        IntRegister currenRegister = (IntRegister) value;
-        if (currenRegister.bits[0].value) {
-            for (int i = 0; i < 32; i++)
-                if (currenRegister.bits[i].value) currenRegister.bits[i].value = false;
-                else currenRegister.bits[i].value = true;
-                Counter.inc(currenRegister);
-        } else {
-            Counter.dec(currenRegister);
-            for (int i = 0; i < 32; i++)
-                if (currenRegister.bits[i].value) currenRegister.bits[i].value = false;
-                else currenRegister.bits[i].value = true;
-        }
-        return currenRegister;
     }
 
     public Register copyRegister() {
@@ -70,8 +26,8 @@ public class IntRegister extends Register {
     public static void main(String[] args) {
         IntRegister intRegister = new IntRegister(25);
         System.out.println(intRegister.toString());
-        System.out.println(intRegister.toTwosComplement(intRegister).toString());
-        System.out.println(intRegister.toTwosComplement(intRegister).toString());
+//        System.out.println(intRegister.toTwosComplement(intRegister).toString());
+//        System.out.println(intRegister.toTwosComplement(intRegister).toString());
     }
 
 }
--- src/ru/progwards/java1/lessons/register2/Register.java
+++ src/ru/progwards/java1/lessons/register2/Register.java
@@ -1,26 +1,55 @@
 package ru.progwards.java1.lessons.register2;
 
 public class Register {
+    Bit[] bits;
+
     public String toString(){
-        return null;
+        String result = "";
+
+        for (Bit bit: this.bits) result += bit.toString();
+        return result;
     }
     public String toDecString() {
-        return null;
-    }
-    public Bit[] getBits() { return null; }
-    public void putRegister(Bit[] value) {}
+        int result = 0;
+        int mult = 1;
+        int n = this.bits.length;
+        for (int i = n - 1; i >= 0; i --){
+            if (i < n - 1) mult *= 2;
+            if (this.bits[i].value) result += mult;
+        }
+        return String.valueOf(result);
 
-    public static int n2Mult(int n) { //возведение 2 в степень n
-        int result = 1;
-        if (n == 0) return 1;
-        for (int i = 0; i < n; i ++) result *= 2;
-        return result;
+    }
+    public Bit[] getBits() {
+        return this.bits;
+    }
+    public void putRegister(Bit[] value) {
+        int n = value.length;
+        for (int i = 0; i < n; i ++) this.bits[i] = value[i];
     }
 
     public Register copyRegister() {return null;}
-    public Register toTwosComplement(Register value){return null;}
+    public Register toTwosComplement(Register value) {
+        Register currenRegister = value;
+        int n = value.bits.length;
+        if (currenRegister.bits[0].value) {
+            for (int i = 0; i < n; i++)
+                if (currenRegister.bits[i].value) currenRegister.bits[i].value = false;
+                else currenRegister.bits[i].value = true;
+            Counter.inc(currenRegister);
+        } else {
+            Counter.dec(currenRegister);
+            for (int i = 0; i < n; i++)
+                if (currenRegister.bits[i].value) currenRegister.bits[i].value = false;
+                else currenRegister.bits[i].value = true;
+        }
+        return currenRegister;
+    }
 
     public static void main(String[] args) {
-        System.out.println(n2Mult(8));
+        IntRegister intRegister = new IntRegister(128);
+//        System.out.println(intRegister.toString());
+        Bit[] bits = intRegister.getBits();
+        System.out.println(intRegister.toDecString());
     }
 }
