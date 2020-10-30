0c2daf74c8e202779981dca302d8ac1131047416
Andrey <kadyrovas@yandex.ru>
2020-10-30 12:39:11 +0300

    package Project;

--- src/ru/progwards/java2/lessons/calculator/Calculator.java
+++ src/ru/progwards/java2/lessons/calculator/Calculator.java
@@ -26,15 +26,15 @@ public class Calculator {
         while (expression.contains("*") || expression.contains("/")){
             for (int i = 0; i < expression.length(); i ++) {
                 if (expression.substring(i, i + 1).compareTo("*") == 0) {
-                    result = Integer.valueOf(expression.substring(i - 1, 1)) *
-                             Integer.valueOf(expression.substring(i + 1, i + 2));
+                    result = Integer.valueOf(value(expression, i - 1)) *
+                             Integer.valueOf(value(expression,i + 1));
                 expression = expression.substring(0, i - 1) + String.valueOf(result) +
                 expression.substring(i + 2);
                 break;
                 }
                 if (expression.substring(i, i + 1).compareTo("/") == 0) {
-                    result = Integer.valueOf(expression.substring(i - 1, 1)) /
-                            Integer.valueOf(expression.substring(i + 1, i + 2));
+                    result = Integer.valueOf(value(expression, i - 1)) /
+                            Integer.valueOf(value(expression,i + 1));
                     expression = expression.substring(0, i - 1) + String.valueOf(result) +
                             expression.substring(i + 2);
                     break;
@@ -46,15 +46,15 @@ public class Calculator {
         while (expression.contains("+") || expression.contains("-")){
             for (int i = 0; i < expression.length(); i ++) {
                 if (expression.substring(i, i + 1).compareTo("+") == 0) {
-                    result = Integer.valueOf(expression.substring(i - 1, 1)) +
-                            Integer.valueOf(expression.substring(i + 1, i + 2));
+                    result = Integer.valueOf(value(expression, i - 1)) +
+                            Integer.valueOf(value(expression,i + 1));
                     expression = expression.substring(0, i - 1) + String.valueOf(result) +
                             expression.substring(i + 2);
                     break;
                 }
                 if (expression.substring(i, i + 1).compareTo("-") == 0) {
-                    result = Integer.valueOf(expression.substring(i - 1, 1)) -
-                            Integer.valueOf(expression.substring(i + 1, i + 2));
+                    result = Integer.valueOf(value(expression, i - 1)) -
+                            Integer.valueOf(value(expression,i + 1));
                     expression = expression.substring(0, i - 1) + String.valueOf(result) +
                             expression.substring(i + 2);
                     break;
@@ -67,7 +67,30 @@ public class Calculator {
         return result;
     }
 
+    public static String value(String expression, int index) {
+        int indexStart = 0;
+        int indexEnd = 0;
+        char[] simbol;
+        for (int i = index; i >= 0; i--) {
+            simbol = expression.substring(i, i + 1).toCharArray();
+            if (!Character.isDigit(simbol[0])) {
+                indexStart = i + 1;
+                break;
+            }
+        }
+        for (int i = index; i < expression.length(); i ++) {
+            simbol = expression.substring(i, i + 1).toCharArray();
+            if (!Character.isDigit(simbol[0])) {
+                indexEnd = i;
+                break;
+            }
+        }
+        if (indexEnd == 0) indexEnd = expression.length();
+        return expression.substring(indexStart, indexEnd);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(calculate("(5+3)*2"));
+        System.out.println(calculate("(8+2)*(30+70)"));
     }
 }
